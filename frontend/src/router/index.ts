a957ccf9de2223e820823905ca604faf2d54ad79
Tony Liao <tonyliao@dev.local>
2026-08-22 18:09:25 +0800

    fix: 改回 hash mode 修復 GitHub Pages 首頁空白
    - router 改回 createWebHashHistory() 以相容 GitHub Pages
    - 移除 index.html 中不必要的 redirect script
    - 更新 404.html 使用 hash mode redirect

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StockView from '../views/StockView.vue'
 import WatchlistPage from '../views/WatchlistView.vue'
 
 const router = createRouter({
-  // 使用 history mode
-  history: createWebHistory(),
+  // 使用 hash mode 以相容 GitHub Pages 靜態部署
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
