8792c027a63b96c3969279022982b8ddae72b74b
Tony Liao <tonyliao@dev.local>
2026-08-22 15:36:04 +0800

    fix: 改用 history mode 移除 URL 中的 #

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StockView from '../views/StockView.vue'
 import WatchlistPage from '../views/WatchlistView.vue'
 
 const router = createRouter({
-  // 使用 hash mode 以相容 GitHub Pages 靜態部署
-  history: createWebHashHistory(),
+  // 使用 history mode
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
